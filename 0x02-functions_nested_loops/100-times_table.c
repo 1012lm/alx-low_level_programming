06fefb97f3b543761361890a99f70a4df171dbe4
michael Beyene <lm1012smile@gmail.com>
2023-02-22 00:02:26 -0800

    task 12, print time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,8 +19,8 @@ _putchar(k + '0');
 }
 else if (k < 10 && j != 0)
 {
-_putchar(',');
 _putchar(' ');
+_putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
@@ -41,8 +41,8 @@ _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
 }
-_putchar('\n');
 }
+_putchar('\n');
 }
 }
 }
